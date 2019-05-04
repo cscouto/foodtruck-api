26f8ef00d457c8f2687753eb615c1f90890fdc8c
Tiago Do Couto <cs.couto@outlook.com>
2019-05-04 10:38:25 -0500

    implemented get and update

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -4,6 +4,8 @@ import Restaurant from '../model/restaurant';
 
 export default({ config, db }) => {
   let api = Router();
+
+  //Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -13,6 +15,42 @@ export default({ config, db }) => {
         res.send(err);
       }
       res.json({ 'message': "Successful"});
+    });
+  });
+
+  //Read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  //Read by id
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  //Update
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      restaurant.name = req.body.name;
+      restaurant.save(err => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ 'message': "Restaurant info updated." });
+      });
     });
   });
 
