16b3c04d8073ccf5fa5a754c1220a8d179554301
Tiago Do Couto <cs.couto@outlook.com>
2019-05-28 19:18:59 -0500

    added delete function added review model

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -54,5 +54,17 @@ export default({ config, db }) => {
     });
   });
 
+  //Delete
+  api.delete('/:id', (req, res) => {
+    Restaurant.remove({
+      _id: req.params.id
+    }, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: "Restaurant successfully removed."});
+    });
+  });
+
   return api;
 }
